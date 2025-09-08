2ecb947152089e341fbf0a5bc9835a1ab391f003
haetten <haetten@gmail.com>
2025-09-08 16:08:16 -0300

    db healthcheck

---
+++ db/healthcheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Variáveis (pegam do container)
+HOST="127.0.0.1"
+PORT="5432"
+USER="${POSTGRES_USER}"
+
+# Testa se Postgres responde
+pg_isready -h "$HOST" -p "$PORT" -U "$USER" || exit 1
+
+# Tenta uma query simples para garantir que autenticação funciona
+psql -h "$HOST" -p "$PORT" -U "$USER" -d "${POSTGRES_DB}" -c "SELECT 1;" >/dev/null 2>&1 || exit 1
+
+exit 0
