4ce8f432da029c2b37ce10b168c9edf1e77dce41
haetten <haetten@gmail.com>
2025-09-08 17:33:06 -0300

    .

--- taiga-db/healthcheck.sh
+++ taiga-db/healthcheck.sh
@@ -10,6 +10,7 @@ USER="${POSTGRES_USER}"
 #pg_isready -h "$HOST" -p "$PORT" -U "$USER" || exit 1
 
 # Tenta uma query simples para garantir que autenticação funciona
-psql -h "$HOST" -p "$PORT" -U "$USER" -d "${POSTGRES_DB}" -c "SELECT 1;" >/dev/null 2>&1 || exit 1
+# psql -h "$HOST" -p "$PORT" -U "$USER" -d "${POSTGRES_DB}" -c "SELECT 1;" >/dev/null 2>&1 || exit 1
+psql -h "localhost" -p "5432" -U "taiga" -d "taiga" -c "SELECT 1;"
 
 exit 0
