7e89637738fcb64c3cea64eee183102d7583761b
haetten <haetten@gmail.com>
2025-09-08 17:41:56 -0300

    .

--- taiga-db/healthcheck.sh
+++ taiga-db/healthcheck.sh
@@ -9,10 +9,8 @@ USER="${POSTGRES_USER}"
 # Testa se Postgres responde
 #pg_isready -h "$HOST" -p "$PORT" -U "$USER" || exit 1
 
-echo "User: " $USER
 # Tenta uma query simples para garantir que autenticação funciona
-# psql -h "$HOST" -p "$PORT" -U "$USER" -d "${POSTGRES_DB}" -c "SELECT 1;" >/dev/null 2>&1 || exit 1
-psql -h "localhost" -p "5432" -U "taiga" -d "taiga" -c "SELECT 1;"
+psql -h "$HOST" -p "$PORT" -U "$USER" -d "${POSTGRES_DB}" -c "SELECT 1;" >/dev/null 2>&1 || exit 1
 
 exit 0
 
